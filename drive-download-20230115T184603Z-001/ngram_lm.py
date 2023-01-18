70b502b28b2989a42cf27461ad96e6324b74fd17
hopecris31 <hope.crisafi@gmail.com>
2023-01-18 03:04:51 -0500

    random_char() and made prob() more robust

--- drive-download-20230115T184603Z-001/ngram_lm.py
+++ drive-download-20230115T184603Z-001/ngram_lm.py
@@ -10,19 +10,22 @@ import math, random
 
 COUNTRY_CODES = ['af', 'cn', 'de', 'fi', 'fr', 'in', 'ir', 'pk', 'za']
 
+
 def start_pad(c):
     ''' Returns a padding string of length c to append to the front of text
         as a pre-processing step to building n-grams. c = n-1 '''
     return '~' * c
 
+
 def ngrams(c, text):
     ''' Returns the ngrams of the text as tuples where the first element is
         the length-c context and the second is the character '''
-    #c is the number of characters to consider for the context (ex 2 is trigram)
-    #start index is c, the amount of characters we are going to consider in the model
-    #tuple contains the words that cone before word to find, and word to find, each char separated by a comma
-    #returns a list of tuples
+    # c is the number of characters to consider for the context (ex 2 is trigram)
+    # start index is c, the amount of characters we are going to consider in the model
+    # tuple contains the words that cone before word to find, and word to find, each char separated by a comma
+    # returns a list of tuples
     pass
+
 
 def create_ngram_model(model_class, path, c=2, k=0):
     ''' Creates and returns a new n-gram model trained on the entire text
@@ -32,6 +35,7 @@ def create_ngram_model(model_class, path, c=2, k=0):
         model.update(f.read())
     return model
 
+
 def create_ngram_model_lines(model_class, path, c=2, k=0):
     '''Creates and returns a new n-gram model trained line by line on the
         text found in the path file. '''
@@ -40,6 +44,7 @@ def create_ngram_model_lines(model_class, path, c=2, k=0):
         for line in f:
             model.update(line.strip())
     return model
+
 
 ################################################################################
 # Basic N-Gram Model
@@ -70,53 +75,31 @@ class NgramModel(object):
         for char in range(len(text)):
             self.__vocab.update(text[char])
             text_list.append(text[char])
-        #print(text_list)
+        # print(text_list)
         end = self.__context + 1
-        for i in range(len(text_list)-self.__context):
+        for i in range(len(text_list) - self.__context):
             n_gram_list = text_list[i:end]
             n_gram = ''.join(n_gram_list)
-            #print(n_gram)
+            # print(n_gram)
             if n_gram in self.__ngrams:
                 self.__ngrams[n_gram] += 1
             else:
                 self.__ngrams.update({n_gram: 1})
             end += 1
 
-
     def prob(self, context: str, char: str):
         ''' Returns the probability of char appearing after context '''
-        ngram = context + char
-        if char in self.__vocab:
-            ngram_occurrences = self.__ngrams.get(ngram)
-            if ngram_occurrences is None:
-                ngram_occurrences = 0
-            ngram_occurrences_start_with_context = self.__get_ngram_count_with_first_context(context)
-            return ngram_occurrences / ngram_occurrences_start_with_context
-        else:
-            #print(len(self.__vocab))
-            return 1 / len(self.__vocab)
-
-        """
-        def prob(self, context, char):
-        ''' Returns the probability of char appearing after context '''
-        matching_char_count = 0
-        if context in self.__context_counts:
-            ngram = context, char
-            if ngram in self.__ngrams:
-                matching_char_count = self.__ngrams[ngram]
-            return matching_char_count / self.__context_counts[context]
-        return 1/self.__vocab_size()  
-        
         ngram = context + char
         if context and char in self.__vocab:
             ngram_occurrences = self.__ngrams.get(ngram)
             ngram_occurrences_start_with_context = self.__get_ngram_count_with_first_context(context)
-            return ngram_occurrences / ngram_occurrences_start_with_context
+            try:
+                return ngram_occurrences / ngram_occurrences_start_with_context
+            except Exception:
+                return 0.0
         else:
-            #print(len(self.__vocab))
-            return 1 / len(self.__vocab)  
-        """
-
+            # print(len(self.__vocab))
+            return 1 / len(self.__vocab)
 
     def __get_ngram_count_with_first_context(self, context: str):
         '''get count of ngrams that start with char '''
@@ -131,18 +114,24 @@ class NgramModel(object):
         ''' Returns a random character based on the given context and the 
             n-grams learned by this model '''
         sorted_vocab = sorted(self.__vocab)
-
-
+        random_num = random.random()
+        probability = 0
+        i = 0
+        while probability <= random_num and i < len(sorted_vocab):
+            probability += self.prob(context, sorted_vocab[i])
+            i += 1
+        return sorted_vocab[i - 1]
 
     def random_text(self, length):
         ''' Returns text of the specified character length based on the
             n-grams learned by this model '''
-        pass
+
 
     def perplexity(self, text):
         ''' Returns the perplexity of text based on the n-grams learned by
             this model '''
         pass
+
 
 ################################################################################
 # N-Gram Model with Interpolation
@@ -163,6 +152,7 @@ class NgramModelWithInterpolation(NgramModel):
     def prob(self, context, char):
         pass
 
+
 ################################################################################
 # Your N-Gram Model Experimentations
 ################################################################################
@@ -176,7 +166,8 @@ class NgramModelWithInterpolation(NgramModel):
 
 
 if __name__ == "__main__":
-    m = NgramModel(1,0)
+
+    m = NgramModel(1, 0)
     m.update('abab')
     print(m.get_vocab())
     m.update('abcd')
@@ -184,10 +175,13 @@ if __name__ == "__main__":
     print(m.prob('a', 'b'))
     print(m.prob('~', 'c'))
     print(m.prob('b', 'c'))
+    print('_____')
 
-
+    n = NgramModel(0, 0)
+    n.update('abab')
+    n.update('abcd')
     random.seed(1)
-    #print([m.random_char('') for i in range(25)])
-    #print(m.random_text(25))
-    #n = create_ngram_model(NgramModel, 'shakespeare_input.txt', 6, 0)
-    #print(n.random_text(250))
+    print([n.random_char('') for i in range(25)])
+    # print(m.random_text(25))
+    # n = create_ngram_model(NgramModel, 'shakespeare_input.txt', 6, 0)
+    # print(n.random_text(250))
